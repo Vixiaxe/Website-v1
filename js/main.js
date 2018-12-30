3f46cdbf73fc577910d14db3adc3c1d846b908fc
Tyler <tyl.hardy@gmail.com>
2018-12-30 13:34:38 -0600

    v.02

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("button");
+
+button.addEventListener("click", function() {
+  var h1 = document.getElementById("test");
+
+  h1.textContent = "Test worked";
+});
+const person = {
+  name: "Tyler",
+  age: 28,
+  gender: "Male"
+};
+console.log(person);
