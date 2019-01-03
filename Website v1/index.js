16daa77ada55f9025db536af4e9ec0cfd4f335ed
Vixiaxe <46256068+Vixiaxe@users.noreply.github.com>
2019-01-02 18:14:45 -0600

    Add files via upload

--- Website v1/index.js
+++ Website v1/index.js
@@ -1,4 +1,5 @@
 //math test
+
 var x = 200;
 var y = (x + 200) * 10;
 var target = document.getElementById("target");
@@ -7,6 +8,34 @@ window.addEventListener("load", function() {
   var skill1 = "VMWare";
 
   var target = document.getElementById("target");
-  var node = document.createTextNode(skill1 + " " + y);
+  let node = document.createTextNode(skill1 + " " + y);
   target.appendChild(node);
 });
+
+for (let i = 0; i <= 10; i++) {
+  printnumbers(i);
+
+  function printnumbers(input) {}
+
+  console.log(i);
+}
+
+var Tyler = {
+  name: "Tyler",
+  Gender: "Male"
+};
+
+console.log(Tyler);
+
+var i = 0;
+while (i <= 10) {
+  console.log(i + "<br />");
+  i++;
+  let node = document.createTextNode(i);
+  var breakl = document.createElement("br");
+  target.appendChild(node);
+  target.appendChild(breakl);
+  console.log(node);
+}
+
+function testing() {}
